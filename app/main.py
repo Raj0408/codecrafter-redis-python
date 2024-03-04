05401ac795c40dc020e1fbf100beefbb53bfba96
Rajkumar Kaneriya <VCJ683@motorolasolutions.com>
2024-03-04 18:26:08 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,10 +5,13 @@ import socket
 def main():
     
     print("Logs from your program will appear here!")
-
+    ping = "PING\r\n"
     server_socket = socket.create_server(("localhost", 6379))
-    server_socket.accept() # wait for client
-
+    con , addr = server_socket.accept() # wait for client
+    print("Connected by ",addr)
+    with con:
+        con.recv(1024)
+        con.send(ping.encode())
 
 if __name__ == "__main__":
     main()
