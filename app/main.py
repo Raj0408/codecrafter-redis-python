552bd56dec12b921d078a5c43dd27a1dd50c7c23
Rajkumar Kaneriya <VCJ683@motorolasolutions.com>
2024-03-13 18:11:32 +0530

    Handshake 2 commit

--- app/main.py
+++ app/main.py
@@ -180,7 +180,7 @@ def command_checker(vector2,info):
     elif command == "replconf" or vector2[1] == "capa":
         response = getresponce("OK")
     elif command == "psync":
-        response = f"+FULLRESYNC {info.master_replid} {0}\r\n"
+        response = f"FULLRESYNC {info.master_replid} {0}\r\n"
         response = getresponce(response)
     return response.encode()
 
